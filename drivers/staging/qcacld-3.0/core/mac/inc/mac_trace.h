30067e024d7fcce336201992d418f99223ad366e
lance <carck@foxmail.com>
2017-11-05 13:11:44 +0800

    staging: qcacld-3.0: fix non-debug build
    Change-Id: Ifa75acbdd3f5a316fcb593635585b74150833179

--- drivers/staging/qcacld-3.0/core/mac/inc/mac_trace.h
+++ drivers/staging/qcacld-3.0/core/mac/inc/mac_trace.h
@@ -34,6 +34,11 @@
 #define MAC_TRACE_GET_MODULE_ID(data) ((data >> 8) & 0xff)
 #define MAC_TRACE_GET_MSG_ID(data)       (data & 0xffff)
 
+QDF_STATUS pe_acquire_global_lock(tAniSirLim *psPe);
+QDF_STATUS pe_release_global_lock(tAniSirLim *psPe);
+
+#ifdef TRACE_RECORD
+
 #define eLOG_NODROP_MISSED_BEACON_SCENARIO 0
 #define eLOG_PROC_DEAUTH_FRAME_SCENARIO 1
 
@@ -47,14 +52,19 @@ uint8_t *mac_trace_get_lim_msg_string(uint16_t limMsg);
 uint8_t *mac_trace_get_wma_msg_string(uint16_t wmaMsg);
 uint8_t *mac_trace_get_sme_msg_string(uint16_t smeMsg);
 uint8_t *mac_trace_get_info_log_string(uint16_t infoLog);
+<<<<<<< HEAD
 QDF_STATUS pe_acquire_global_lock(tAniSirLim *psPe);
 QDF_STATUS pe_release_global_lock(tAniSirLim *psPe);
+=======
+
+>>>>>>> 90d55bc2fbb7 (staging: qcacld-3.0: fix non-debug build)
 uint8_t *mac_trace_get_neighbour_roam_state(uint16_t neighbourRoamState);
 uint8_t *mac_trace_getcsr_roam_state(uint16_t csr_roamState);
 uint8_t *mac_trace_getcsr_roam_sub_state(uint16_t csr_roamSubState);
 uint8_t *mac_trace_get_lim_sme_state(uint16_t limState);
 uint8_t *mac_trace_get_lim_mlm_state(uint16_t mlmState);
 uint8_t *mac_trace_get_tl_state(uint16_t tlState);
+<<<<<<< HEAD
 #else
 static inline void mac_trace(tpAniSirGlobal pMac, uint8_t code, uint16_t session,
 			     uint32_t data)
@@ -116,6 +126,24 @@ static inline uint8_t *mac_trace_get_tl_state(uint16_t tlState)
 {
 	return NULL;
 }
+=======
+
+#else
+
+#define mac_trace_get_cfg_msg_string(cfgMsg) "NULL"
+#define mac_trace_get_lim_msg_string(limMsg) "NULL"
+#define mac_trace_get_wma_msg_string(wmaMsg) "NULL"
+#define mac_trace_get_sme_msg_string(smeMsg) "NULL"
+#define mac_trace_get_info_log_string(infoLog) "NULL"
+
+#define mac_trace_get_neighbour_roam_state(neighbourRoamState) "NULL"
+#define mac_trace_getcsr_roam_state(csr_roamState) "NULL"
+#define mac_trace_getcsr_roam_sub_state(csr_roamSubState) "NULL"
+#define mac_trace_get_lim_sme_state(limState) "NULL"
+#define mac_trace_get_lim_mlm_state(mlmState) "NULL"
+#define mac_trace_get_tl_state(tlState) "NULL"
+
+>>>>>>> 90d55bc2fbb7 (staging: qcacld-3.0: fix non-debug build)
 #endif
 
 #endif
